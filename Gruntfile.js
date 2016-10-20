25c85d5b0ebe04c11cf7af3aa2738fe3a2b6ef17
Michael Wood <mwood@markettraders.com>
2016-10-20 16:25:55 -0400

    Initial commit. Build process still not working

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		"babel": {
+			options: {
+				plugins: ['transform-react-jsx'], // npm install babel-plugin-transform-react-jsx
+				presets: ['es2015', 'react'] // npm install babel-preset-es2015 babel-preset-react
+			},
+			dist: {
+				files: [{
+					"expand": true,
+					"cwd": "src/jsx/",
+					"src": ["**/*.jsx"],
+					"dest": "build/js",
+					"ext": ".js"
+				}]
+			}
+		},
+		concat: {
+	    options: {
+	      separator: ';',
+	    },
+	    js: {
+	      src: ['build/js/Slide.js', 'build/js/Slider.js'],
+	      dest: ['dist/js/image-slider.js'],
+	    },
+	  },
+		min: {
+			js: {
+				src: "dist/js/image-slider.js",
+				dest: "dist/js/image-slider.min.js"
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-babel');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask("default", ["babel", "concat"]);
+};
---
+++ src/jsx/components/Slider.jsx
@@ -0,0 +1,12 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var Slider = React.createClass({
+  render: function() {
+    return (
+      <h1>Test</h1>
+    );
+  }
+});
+
+ReactDOM.render(<Slide/>, document.getElementById("image-slider"));
